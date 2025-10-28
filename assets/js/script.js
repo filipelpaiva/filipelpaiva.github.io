262aa4acb79d82a60295a9f72ef693ab61df8253
filipelpaiva <fpaiva@junifeup.pt>
2025-10-28 16:41:00 +0000

    favicon

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,8 +59,8 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
-            $("#favicon").attr("href", "assets/images/favicon.png");
+            document.title = "Portfolio | Filipe Paiva";
+            $("#favicon").attr("href", "assets/images/favicon_xxx.png");
         }
         else {
             document.title = "Come Back To Portfolio";
